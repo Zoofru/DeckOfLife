8cb0335640ccd2ab15b378c0ccc6bc36c0330cd2
Zoofru <masonbush752@gmail.com>
2022-05-09 16:21:51 -0500

    added back scrollbar, for laptop users. login form update

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,8 +1,13 @@
 import { useState } from "react"
+import { Link } from "react-router-dom"
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
 
 const Login = () => {
     const [password, setPassword] = useState('')
     const [email, setEmail] = useState('')
+    const [rememberMeChecked, setRememberMeChecked] = useState(false)
 
     const handleLogin = async e => {
         e.preventDefault()
@@ -13,6 +18,12 @@ const Login = () => {
         setEmail('')
     }
 
+    const Styles = {
+        checkBoxText: {
+            color: '#0062FF',
+        },
+    }
+
     return(
         <div className="login-page">
             <div className="login-img">
@@ -20,7 +31,7 @@ const Login = () => {
             </div>
 
             <div className="login-info">
-                <h1 className="font login-title">Deck Of Life</h1>
+                <h1 className="font login-title">Welcome, <br /> <span className='blue-neon'>Card Chaser</span></h1>
                 <form className="login-form" onSubmit={handleLogin}>
 
                     <input 
@@ -29,21 +40,45 @@ const Login = () => {
                         autoComplete="false" 
                         spellCheck='false' 
                         placeholder="-> Email" 
-                        value={email} 
+                        value={email}
+                        required
                         onChange={e => { setEmail(e.target.value) }}
                     />
 
                     <input 
-                        className="login-from-password-input login-input font" 
+                        className="login-from-password-input login-input font input-password" 
                         type='password' 
                         autoComplete="false" 
                         spellCheck='false' 
                         placeholder="-> Password" 
-                        value={password} 
+                        value={password}
+                        required
                         onChange={e => { setPassword(e.target.value) }} 
                     />
+                    
+                    <div className="user-actions-login">
+                        <FormGroup>
+                            <FormControlLabel 
+                                control={
+                                    <Checkbox 
+                                        defaultChecked 
+                                        sx={{color: "#0062FF"}} 
+                                        size="small"
+                                        onChange={e => setRememberMeChecked(!rememberMeChecked) }
+                                    />
+                                } 
+                                label="Remember me" 
+                                style={Styles.checkBoxText}
+                            />
+                        </FormGroup>
 
-                    <button className="login-form-submit font" type="submit">Submit</button>
+                        <Link to='/forgot-password' className="forgot-password">
+                            <p>Forget password?</p>
+                        </Link>
+
+                    </div>
+
+                    <button className="login-form-submit font" type="submit">Sign In</button>
                 </form>
             </div>
         </div>
