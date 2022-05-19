6dc812e0b8e9e44aa6977db3214d85ba958327e1
Zoofru <masonbush752@gmail.com>
2022-05-18 19:59:36 -0500

    Icons added to navbar

--- src/userhomepage/left/nav.js
+++ src/userhomepage/left/nav.js
@@ -1,12 +1,17 @@
 import { Link, useNavigate } from "react-router-dom"
 import './nav.scss'
-
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import SportsEsportsRoundedIcon from '@mui/icons-material/SportsEsportsRounded';
+import PersonRoundedIcon from '@mui/icons-material/PersonRounded';
+import SettingsIcon from '@mui/icons-material/Settings';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const Nav = props => {
     const navigate = useNavigate()
 
     const logoutUser = () => {
         localStorage.removeItem('UAT')
+        localStorage.removeItem('CUSER')
         navigate('/')
     }
 
@@ -16,12 +21,27 @@ const Nav = props => {
         if(item === 'Logout') {
             return(
                 <div className="nav-item" key={index} onClick={logoutUser}>
-                    <h5 className="nav-item-text white font-roboto">{item}</h5>
+                    <LogoutIcon className="icon-nav"></LogoutIcon>
+                    <h6 className="nav-item-text white font-roboto">{item}</h6>
                 </div>
             )
         } else {
             return(
                 <div className="nav-item" key={index}>
+                    {item === 'Home' ? 
+                        <HomeRoundedIcon className="icon-nav"></HomeRoundedIcon>
+                    :
+                        item === 'Play' ?
+                            <SportsEsportsRoundedIcon className='icon-nav'></SportsEsportsRoundedIcon>
+                        :
+                            item === 'Profile' ?
+                                <PersonRoundedIcon className='icon-nav'></PersonRoundedIcon>
+                            :
+                                item === 'Settings' ?
+                                    <SettingsIcon className="icon-nav"></SettingsIcon>
+                                :
+                                <></>
+                    }
                     <Link to={`/${item}`} className='hideLink'>
                         <h5 className="nav-item-text white font-roboto">{item}</h5>
                     </Link>
