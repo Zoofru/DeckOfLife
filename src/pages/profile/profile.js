d9f732ee1717f825a0282748c4f8ac300a36e21f
Zoofru <masonbush752@gmail.com>
2022-05-18 21:01:04 -0500

    added profile and settings pages and corresponding scss files.

---
+++ src/pages/profile/profile.js
@@ -0,0 +1,9 @@
+const Profile = props => {
+    return(
+        <div className="profile">
+
+        </div>
+    )
+}
+
+export default Profile
---
+++ src/pages/settings/usersettingspage.js
@@ -0,0 +1,9 @@
+const UserSettingsPage = props => {
+    return(
+        <div className="settings-page">
+
+        </div>
+    )
+}
+
+export default UserSettingsPage
